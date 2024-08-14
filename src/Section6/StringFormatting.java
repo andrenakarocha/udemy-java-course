ef8211c892d22bfab129166e3216c66913f6e4bc
André Rocha <andrenakarocha@hotmail.com>
2024-08-14 16:23:04 -0300

    String Formatting - Section 6

---
+++ src/Section6/StringFormatting.java
@@ -0,0 +1,23 @@
+package Section6;
+
+public class StringFormatting {
+    public static void main(String[] args) {
+        // Para formatar uma string, primeiro eu escrevo ela com as partes que desejo formatar:
+        String bemVindoTemplate = "Olá, %s! Boa %s";
+        // Depois crio as minhas opções de strings:
+        String manha = "manhã";
+        String tarde = "tarde";
+        String noite = "noite";
+        String nome = "André";
+
+        // E formato ela:
+        String stringFormatada = String.format(bemVindoTemplate, nome, tarde);
+        System.out.println(stringFormatada);
+
+        // Caso eu use o prinf, posso passar %d onde eu posso colocar qualquer digito que eu desejar
+        System.out.printf("Você tem %d computadores disponíveis na loja!", 10);
+
+        // Tem muitas outras formatações:
+        // https://docs.oracle.com/javase/8/docs/api/java/util/Formatter.html
+    }
+}

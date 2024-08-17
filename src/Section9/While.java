37f407d5398cc05d096b058f65b723569d7fd4c8
André Rocha <andrenakarocha@hotmail.com>
2024-08-17 09:24:37 -0300

    While - Section 9

---
+++ src/Section9/While.java
@@ -0,0 +1,17 @@
+package Section9;
+
+public class While {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i < 5) {
+            System.out.println("Contador = " + i);
+            i ++
+        }
+
+        // Loop infinito:
+//      while (true){
+//
+//      }
+    }
+}

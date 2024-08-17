15422309c36f810e38fe32d5267849bd7b1e76c9
André Rocha <andrenakarocha@hotmail.com>
2024-08-17 09:19:45 -0300

    Random Numbers - Section 8

---
+++ src/Section8/RandomNumbers.java
@@ -0,0 +1,13 @@
+package Section8;
+
+import java.util.Random;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        Random r = new Random();
+
+        System.out.println(r.nextInt()); // qualquer int aleatório
+        System.out.println(r.nextInt(100)); // qualquer int entre 0 e 99
+        System.out.println(r.nextInt(100) + 1); // qualquer int entre 0 e 100
+    }
+}

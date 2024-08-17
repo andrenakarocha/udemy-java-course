ac0da79a543cb2733d63a53e62e3fc621690da6c
André Rocha <andrenakarocha@hotmail.com>
2024-08-17 09:51:11 -0300

    Nested Loops - Section 9

---
+++ src/Section9/NestedLoops.java
@@ -0,0 +1,13 @@
+package Section9;
+
+public class NestedLoops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Primeiro loop i = " + i);
+            // Nested loop:
+            for (int j = 0; j < 3; j++) {
+                System.out.println("Segundo loop j = " + j);
+            }
+        }
+    }
+}

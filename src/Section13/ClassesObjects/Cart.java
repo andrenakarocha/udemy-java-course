4d3eb11d5ea6a0995fbe6c903463ecc7af749cf6
André Rocha <andrenakarocha@hotmail.com>
2024-08-24 11:57:42 -0300

    Classes & Objects - Section 13 - (Starting class)

---
+++ src/Section13/ClassesObjects/Cart.java
@@ -0,0 +1,14 @@
+package Section13.ClassesObjects;
+
+public class Cart {
+    private static final int DEFAULT_CART_CAPACITY = 10;
+    private static final int MONEY_SCALE = 2;
+    private static final double DEFAULT_TAX_RATE = 0.15;
+    private static final String DEFAULT_TAX_TYPE = "incomeTax";
+    private static final double DEFAULT_DISCOUNT_RATE = 0;
+    private static final String DEFAULT_DISCOUNT_NAME = "zeroDiscount";
+
+    private static int cartCounter;
+
+
+}
